91fd1f32e1519338b165d88cf0d572437e2ddaa9
uubek <uubeks2@gmail.com>
2022-06-04 00:08:14 +0500

    User controller updated

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function index(Request $request)
     {
-        $user = $request->user()->with('Companies');
+        $user = User::with('Companies.Projects')->find($request->user()->id);
 
         return response()->json($user);
     }
