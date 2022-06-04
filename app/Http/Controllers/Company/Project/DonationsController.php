6b7e3b0f139b8708ffc9232f1209a3cbe7938da4
Muhamnmadiy <86729207+MuhammadiyDev@users.noreply.github.com>
2022-06-04 11:14:09 +0500

    optimized donation create

--- app/Http/Controllers/Company/Project/DonationsController.php
+++ app/Http/Controllers/Company/Project/DonationsController.php
@@ -26,7 +26,7 @@ class DonationsController extends Controller
 
         $project = Project::where('alias', $alias)->first();
 
-        if ($validated['pane_id']) {
+        if ($validated['plan_id']) {
             $donate = $project->Plans()->find($validated['plan_id'])->Donations()->create([
                 'project_id' => $project->id,
                 'user_id' => $request->user()->id,
@@ -35,7 +35,7 @@ class DonationsController extends Controller
             ]);
         } else {
             $donate = Donation::create([
-                'plane_id' => null,
+                'plan_id' => null,
                 'project_id' => $project->id,
                 'user_id' => $request->user()->id,
                 'amount' => $validated['amount'],
