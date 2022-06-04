c4ce34462d9823b7d2ea9d8dccc924a79c6b49a8
uubek <uubeks2@gmail.com>
2022-06-04 10:34:26 +0500

    Donatoins create removed

--- app/Http/Controllers/Company/Project/DonationsController.php
+++ app/Http/Controllers/Company/Project/DonationsController.php
@@ -13,27 +13,7 @@ class DonationsController extends Controller
 {
     public function create($alias, Request $request)
     {
-        $project = Project::with('Plans')->where('alias', $alias)->first();
-        $user = $request->user();
 
-        $validator = Validator::make($request->all(), [
-            'plan_id' => 'required|integer',
-            'amount' => 'required|integer',
-        ]);
-
-        if ($validator->fails()) {
-            throw new ValidationException($validator->errors());
-        }
-
-        $validated = $validator->validated();
-
-        Donation::create([
-            'user_id' => $user->id,
-            'plan_id' => $validated['plan_id'],
-            'amount' => $validated(['amount']),
-        ]);
-
-        return response()->json(['error' => null]);
     }
 
      public function index($alias)
