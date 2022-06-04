190a37b1061dea58ed534803d1e4b117b62591b5
Muhamnmadiy <86729207+MuhammadiyDev@users.noreply.github.com>
2022-06-04 10:07:37 +0500

    updated router for method update company

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,7 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
 
         // Router for Company
         Route::post('/company/register', [\App\Http\Controllers\Company\CompanyController::class, 'create']);
-        Route::post('/company/update/{alias}', [\App\Http\Controllers\Company\CompanyController::class, 'update']);
+        Route::post('/company/{alias}/update', [\App\Http\Controllers\Company\CompanyController::class, 'update']);
     });
 
     // Router for logout user
